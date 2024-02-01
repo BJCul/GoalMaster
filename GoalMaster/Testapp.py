be8f6d1821a962e627d02ca05ef69f2c43d3e116
BJCul <92005368+BJCul@users.noreply.github.com>
2024-02-02 02:26:11 +0800

    Update Testapp.py

--- GoalMaster/Testapp.py
+++ GoalMaster/Testapp.py
@@ -3,12 +3,14 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivymd.theming import ThemeManager
+from kivy.uix.boxlayout import BoxLayout
+
 
 Window.size = (360, 640)
 
 class LoginScreen(Screen):
     def __init__(self, **kwags):
-        super().__init__(**kwags)
+        super().__init__(**kwags) 
     pass
 
 class StartupScreen(Screen):
@@ -34,8 +36,15 @@ class SignupScreen(Screen):
 class DashboardScreen(Screen):
     def __init__(self, **kwags):
         super().__init__(**kwags)
-    pass
 
+    def toggle_nav_drawer(self):
+        self.ids.top_app_bar.ids.nav_drawer.toggle_nav_drawer()
+
+    def item_selected(self, text):
+        print(f'Item selected: {text}')
+    
+class ContentNavigationDrawer(BoxLayout):
+        pass
 
 screen_manager = ScreenManager()
 # Adding screens to the screen manager
@@ -46,7 +55,7 @@ screen_manager.add_widget(LoginScreen(name='login'))
 screen_manager.add_widget(LoginScreen(name='signup'))
 screen_manager.add_widget(DashboardScreen(name='dashboard'))
 
-    
+
 class MyApp(MDApp):
     def build(self):
         self.theme_cls.primary_palette = "BlueGray"
